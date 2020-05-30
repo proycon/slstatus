638179d8a9c4e8b88b873dc2cb1869fbe0d33166
Maarten van Gompel <proycon@anaproy.nl>
2020-05-30 12:06:13 +0200

    added my config

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 const unsigned int interval = 1000;
 
 /* text to show if no value can be retrieved */
-static const char unknown_str[] = "n/a";
+static const char unknown_str[] = "";
 
 /* maximum output string length */
 #define MAXLEN 2048
@@ -63,5 +63,11 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+    { keymap, "🌍 %s ", NULL },
+    //{ wifi_essid, "🖧 %s", "wlan0" },
+    //{ battery_perc, "🔋 %s", "BAT0" },
+    { run_command, "🔊 %s ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ cpu_perc, "cpu %s%% ", NULL	      },
+	{ ram_perc, "ram %s%% ", NULL	      },
+	{ datetime, "%s", "🕑 %H:%M" },
 };
